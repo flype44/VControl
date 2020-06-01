e5b095d053d5ddd0d8bcad46e3d745c37f35d273
flype44 <flype44@gmail.com>
2020-06-02 00:27:07 +0200

    Minor cosmetics

--- V_CPU_CLK.asm
+++ V_CPU_CLK.asm
@@ -32,16 +32,16 @@ _v_cpu_multiplier:
 	
 	movem.l  d2-d7/a2-a6,-(sp)
 	
-    lea.l    SoftIntData(pc),a4
-
-    lea.l    DOSName(pc),a1
-    moveq.l  #0,d0
-    move.l   $4.w,a6
-    jsr      _LVOOpenLibrary(a6)
-    move.l   d0,a3
-    tst.l    d0
-    beq.w    .exit
-
+	lea.l    SoftIntData(pc),a4
+	
+	lea.l    DOSName(pc),a1
+	moveq.l  #0,d0
+	move.l   $4.w,a6
+	jsr      _LVOOpenLibrary(a6)
+	move.l   d0,a3
+	tst.l    d0
+	beq.w    .exit
+	
 	lea.l    MySoftInt(pc),a0
 	lea.l    SoftIntCode(pc),a1
 	move.l   a1,IS_CODE(a0)
@@ -58,12 +58,12 @@ _v_cpu_multiplier:
 	move.b   #NT_MSGPORT,LN_TYPE(a0)
 	move.b   #PA_SOFTINT,MP_FLAGS(a0)
 	move.l   a1,MP_SIGTASK(a0)
-
+	
 	lea.l    MyTimeReq(pc),a0
 	move.b   #NT_REPLYMSG,LN_TYPE(a0)
 	move.w   #IOTV_SIZE,MN_LENGTH(a0)
 	move.l   sid_MsgPort(a4),MN_REPLYPORT(a0)
-
+	
 	lea.l    TimerName(pc),a0
 	moveq.l  #UNIT_MICROHZ,d0
 	lea.l    MyTimeReq(pc),a1
@@ -71,7 +71,7 @@ _v_cpu_multiplier:
 	jsr      _LVOOpenDevice(a6)
 	tst.l    d0
 	bne.s    .delport
-
+	
 	move.l   #1,sid_Running(a4)
 	lea.l    MyTimeReq(pc),a1
 	move.w   #TR_ADDREQUEST,IO_COMMAND(a1) 
@@ -92,11 +92,11 @@ _v_cpu_multiplier:
 	move.l   $4.w,a6
 	lea.l    MyTimeReq(pc),a1
 	jsr      _LVOCloseDevice(a6)
-    
+
 .delport
 	move.l   sid_MsgPort(a4),a0
 	jsr      _LVODeleteMsgPort(a6)
-    
+
 .closedos
 	move.l   a3,a1
 	jsr      _LVOCloseLibrary(a6)
@@ -130,15 +130,21 @@ SoftIntCode:
 
 	cnop 0,4
 
-MySoftInt    ds.b IS_SIZE
-MyTimeReq    ds.b IOTV_SIZE
-SoftIntData  ds.b sid_SIZE
+MySoftInt:
+	ds.b IS_SIZE
 
-	cnop 0,4
+MyTimeReq:
+	ds.b IOTV_SIZE
 
-DOSName      DOSNAME
-TimerName    TIMERNAME
+SoftIntData:
+	ds.b sid_SIZE
 
-	cnop 0,4
+DOSName:
+	DOSNAME
+
+TimerName:
+	TIMERNAME
 
 ;----------------------------------------------------------
+
+	cnop 0,4
