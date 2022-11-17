788191a16d9386633d26078e49e518b45691a323
Renaud Schweingruber <TuKo1982@users.noreply.github.com>
2022-11-17 17:35:33 +0100

    Update VControl.h

--- VControl.h
+++ VControl.h
@@ -16,13 +16,12 @@
 #define VREG_BOARD_Unknown  0x00
 #define VREG_BOARD_V600     0x01
 #define VREG_BOARD_V500     0x02
-#define VREG_BOARD_V4       0x03
-#define VREG_BOARD_V666     0x04
+#define VREG_BOARD_V4FB     0x03
+#define VREG_BOARD_V4ID     0x04
 #define VREG_BOARD_V4SA     0x05
 #define VREG_BOARD_V1200    0x06
-#define VREG_BOARD_V4000    0x07
-#define VREG_BOARD_VCD32    0x08
-#define VREG_BOARD_Future   0x09
+#define VREG_BOARD_V4MC     0x07
+#define VREG_BOARD_Future   0x08
 
 /*****************************************************************
  * EXTERNAL FUNCTIONS
